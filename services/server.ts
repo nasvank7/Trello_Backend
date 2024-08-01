9348fbec51d98d44ff775890076c02ac5351db5b
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-08-01 16:17:53 +0530

    Update server.ts

--- services/server.ts
+++ services/server.ts
@@ -2,16 +2,21 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import client from "./connection/db";
 import userRoute from "./routes/userRouter";
+import cors from 'cors'
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
+
 
 client.connect(function (err) {
   if (err) throw err;
   console.log("Connected!");
 });
 
+app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/user", userRoute);
