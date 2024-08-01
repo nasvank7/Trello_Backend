39f0bf3aa9c7239eaa7447e7c95989bd4ef06399
nasvank7 <122220975+nasvank7@users.noreply.github.com>
2024-08-01 16:18:18 +0530

    Update userController.ts

--- services/controller/userController.ts
+++ services/controller/userController.ts
@@ -2,20 +2,63 @@ import { Express, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
 import client from "../connection/db";
+import { generateJwt } from "../services/generateJWT";
 const prisma = new PrismaClient();
 const userController = {
   PostLogin: async (req: Request, res: Response) => {
     try {
-      const { email, password } = req.body;
-    } catch (error) {}
+      const { email, password, username } = req.body;
+
+      if (!email && !username) {
+        return res.status(400).json({ message: "Username or Email is required" });
+      }
+      if (!password) {
+        return res.status(400).json({ message: "Password is required" });
+      }
+
+      const user = await prisma.user.findFirst({
+        where: {
+          OR: [
+            { email: email || '' },
+            { username: username || '' }
+          ]
+        }
+      });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      // Check password
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid credentials" });
+      }
+
+      const token = generateJwt(user.username || user.email)
+      console.log(token);
+
+      if (token) {
+        res.cookie('jwt', token, {
+          httpOnly: true, 
+          secure: process.env.NODE_ENV === 'production', 
+          sameSite: 'strict', // 'strict', 'lax', or 'none'
+        });
+      }
+
+      res.status(200).json({ message: "Login successful", user,token:token });
+    } catch (error) {
+      console.error('Login error:', error);
+      res.status(500).json({ message: "Internal server error" });
+    }
   },
   PostRegister: async (req: Request, res: Response): Promise<void> => {
-    const { email, password, username } = req.body;
+    const { email, password, confirmPassword, username } = req.body;
     console.log(req.body);
 
     try {
       // Check if email and password are provided
-      if (!email || !password) {
+      if (!email || !password || !confirmPassword) {
         res.status(400).json({ message: "Email and password are required." });
         return;
       }
@@ -39,6 +82,7 @@ const userController = {
           username,
           email,
           password: hashedPassword,
+          confirmPassword: hashedPassword
         },
       });
 
